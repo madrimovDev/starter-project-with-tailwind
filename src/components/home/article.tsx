9f06e69eef1c8f2162154a5cfb57f613a623ea57
madrimovDev <madrimov5014@gmail.com>
2022-07-04 16:12:07 +0500

    Fixing minibug

--- src/components/home/article.tsx
+++ src/components/home/article.tsx
@@ -6,7 +6,7 @@ const Article = () => {
     <div className='mt-4 min-h-[80vh] flex flex-col justify-center items-center'>
       <div className='text-center'>
         <h2 className='uppercase text-brand-500 mb-2'>Development Team</h2>
-        <h1 className='text-3xl md:text-4xl tracking-wide'>Deebcode Software</h1>
+        <h1 className='text-3xl md:text-4xl tracking-wide'>Deepcode Software</h1>
       </div>
 
     </div>
